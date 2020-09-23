5c83a9da844ee085f68e4b05c7476ee8d3f65cc3
limbeltrana <63408600+limbeltrana@users.noreply.github.com>
2020-09-22 23:07:29 -0500

    Updates

---
+++ precache-manifest.31139ad440418912b7de54def35462e0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4935918fc7ca7a6b4b9bda5ac7027241",
+    "url": "/%7Bstore-beltran-lina%7D/index.html"
+  },
+  {
+    "revision": "9885e2667be0317b0234",
+    "url": "/%7Bstore-beltran-lina%7D/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c51f9ba0fcda88f8455b",
+    "url": "/%7Bstore-beltran-lina%7D/static/css/main.d2b6eedc.chunk.css"
+  },
+  {
+    "revision": "9885e2667be0317b0234",
+    "url": "/%7Bstore-beltran-lina%7D/static/js/2.013eaac1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/%7Bstore-beltran-lina%7D/static/js/2.013eaac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51f9ba0fcda88f8455b",
+    "url": "/%7Bstore-beltran-lina%7D/static/js/main.08f701ef.chunk.js"
+  },
+  {
+    "revision": "64146c882efc94ebdcff",
+    "url": "/%7Bstore-beltran-lina%7D/static/js/runtime-main.d12de9a3.js"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/aerolab-logo.71af6f4b.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "251b737ea018817d8e9eb384b6933f18",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/failImg.251b737e.gif"
+  },
+  {
+    "revision": "a343e15346be343af7867fa8f95ad9d5",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/header-x2.a343e153.png"
+  },
+  {
+    "revision": "1379183c20394a2a0aa262496a58de19",
+    "url": "/%7Bstore-beltran-lina%7D/static/media/succesImg.1379183c.gif"
+  }
+]);
